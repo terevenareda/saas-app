2b797ec09537d4098c9f2db896d2b3f76a6d583c
terevenareda <terevenathomas2018@gmail.com>
2025-10-26 21:45:49 +0300

    initial

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,8 @@
+const NewCompanion = () => {
+    return (
+        <div>NewCompanion</div>
+    )
+}
+
+export default NewCompanion
+
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>Sign In</div>
+    )
+}
+
+export default Page
+
